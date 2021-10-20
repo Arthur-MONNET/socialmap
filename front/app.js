7dd9c54465c8aebd072ef691e0c221b58fe70049
Roxane-gll <guella.roxane@gmail.com>
2021-10-20 08:46:49 +0200

    create mapbox

--- front/app.js
+++ front/app.js
@@ -5,7 +5,7 @@ var path = require('path')
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/',function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/mapbox.html');
     res.send()
 });
 app.listen(3000)
