271d62e97c71e076cad173a03090a73e1ac3f617
Roxane-gll <guella.roxane@gmail.com>
2021-11-02 16:32:57 +0100

    delete map box token

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ Grinwitch + longitude /90*500   |    Equator + latitude /90*500
 var express = require('express');
 var app = express();
 var path = require('path')
+require('dotenv').config()
 
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/index.html');
     res.send()
+});
+
+app.get('/', function(req, res){
+    res.render('./pages/map.html',{mapToken:process.env.MAP_TOKEN});
 });
 app.listen(3000)
