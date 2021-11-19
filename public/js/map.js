2de2117b283e3aa7f3b63d51bc2a162b0d0fb4e5
Roxane-gll <guella.roxane@gmail.com>
2021-11-19 13:44:25 +0100

    add retweets

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,8 @@
+const search = document.getElementById('search')
+const searchInput = document.getElementById('searchInput')
+const tweetsDiv = document.getElementById('tweets')
+let tweetsInsered = document.getElementsByClassName('tweetI')
+
 $.ajax({
     url: "/mapToken",
     type: "POST",
@@ -13,8 +18,6 @@ async function addMarker(nbTweets, token) {
     const temp = await getUser()
     const geocodeUser = await getAdressGeocode(token, JSON.parse(temp).data.location)
     const temp2 = await getTweetsUser(JSON.parse(temp).data.id)
-    const retweets = await quotedOf(temp2._realData.data[nbTweets].id)
-    console.log('retweets', 'tweets', nbTweets, retweets._realData)
     if (temp2._realData.includes.places[nbTweets].geo.bbox[0]) {
         return [temp2._realData.includes.places[nbTweets].geo.bbox[0], temp2._realData.includes.places[nbTweets].geo.bbox[1]]
     } else {
@@ -25,7 +28,7 @@ async function addMarker(nbTweets, token) {
 async function addPopup(nbTweets) {
     const temp = await getUser()
     const temp2 = await getTweetsUser(JSON.parse(temp).data.id)
-    const retweets = await quotedOf(temp2._realData.data[nbTweets].id)
+    /* const retweets = await quotedOf(temp2._realData.data[nbTweets].id)
     let placeRetweets = ''
     if (retweets._realData.includes.places) {
         for (const place in retweets._realData.includes.places) {
@@ -36,9 +39,9 @@ async function addPopup(nbTweets) {
         for (let i = 0; i < retweets._realData.includes.users.length; i++) {
             placeRetweets += retweets._realData.includes.users[i].location
         }
-    }
+    } */
     return {'HTML': `<em>${temp2._realData.data[nbTweets].text}</em><br>
-            Retweet: ${temp2._realData.data[nbTweets].public_metrics.retweet_count+temp2._realData.data[nbTweets].public_metrics.quote_count}, "<em>${placeRetweets}</em>"<br>
+            Retweet: ${temp2._realData.data[nbTweets].public_metrics.retweet_count+temp2._realData.data[nbTweets].public_metrics.quote_count}<br>
             Like: ${temp2._realData.data[nbTweets].public_metrics.like_count}<br>
             Reponse: ${temp2._realData.data[nbTweets].public_metrics.reply_count}
         `,'id': temp2._realData.data[nbTweets].id }
@@ -58,13 +61,17 @@ function drawMap (response) {
         style: 'mapbox://styles/zak74/ckuzm2lot0yjg14s4e5k4202y',
         center: [5, 45],
         zoom: 4.5
-    });
+    })
     let hoveredCountryId = null
     map.on('load', () => {
         map.dragRotate.disable()
     
         // disable map rotation using touch rotation gesture
         map.touchZoomRotate.disableRotation()
+
+        /**
+         * Initialize map and 
+         */
 
         map.addSource('country', {
             type: 'geojson',
@@ -133,28 +140,82 @@ function drawMap (response) {
         map.on('mouseenter', 'country-fills', () => {
             map.getCanvas().style.cursor = 'pointer';
         })
+
+        // add marker for tweets 
         async function addMarkers() {
             const nbTweets = JSON.parse(await getUser()).data.public_metrics.tweet_count
-            
             for (let i=0; i<nbTweets; i++) {
                 const popupTextId = await addPopup(i)
+                tweetsDiv.insertAdjacentHTML('afterBegin', `<div class="tweetI" id="${popupTextId.id}"><p>${popupTextId.HTML}</p></div>`)
                 const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
                     popupTextId.HTML
                 )
-                const el = document.createElement('div');
-                el.id = 'tweets';
+                const el = document.createElement('div')
+                el.id = 'tweets'
                 var marker = new mapboxgl.Marker(el)
                     .setLngLat(await addMarker(i, response))
                     .setPopup(popup)
                     .addTo(map)
             }
+            addClickTweets()
         }
         addMarkers()
+        search.addEventListener('click', (e) =>{
+            console.log(searchInput.value)
+        })
+        let retweetsList = []
+        function addClickTweets() {
+            retweetsList = []
+            tweetsInsered = document.getElementsByClassName('tweetI')
+            const arrayTweetsDiv = Array.prototype.slice.call( tweetsInsered )
+            arrayTweetsDiv.forEach(el => {
+                el.addEventListener('click', async () =>{
+                    await addRetweetsMarker(el.id)
+                    console.log(retweetsList)
+                    el.style.backgroundColor= '#1DA9B9'
+                    for (let i = 0; i < arrayTweetsDiv.length; i++) {
+                        if (arrayTweetsDiv[i].id !== el.id) {
+                            arrayTweetsDiv[i].style.backgroundColor= 'tomato'
+                        }
+                    }
+                })
+            })
+        }
+
+        async function addRetweetsMarker(id) {
+            const retweets = await quotedOf(id)
+            let locationRetweet = ''
+            let all = {}
+            retweets._realData.data.forEach((l, i) => {
+                if (retweets._realData.includes.places) {
+                    locationRetweet = retweets._realData.includes.places[i]
+                } else if(retweets._realData.includes.users[i].location) {
+                    locationRetweet = retweets._realData.includes.users[i].location
+                }
+                all.location = locationRetweet
+                all.user = retweets._realData.includes.users[i].name
+                all.user = l.text
+                console.log('retweets', all)
+                return addMarkerRetweet(all)
+            })
+            async function addMarkerRetweet(object) {
+                if (typeof object.location !== Array) {
+                    object.location = await getAdressGeocode(response, object.location)
+                    object.location = object.location.center
+                }
+                console.log(object)
+                return retweetsList.push(object)
+            }
+            
+        }
     })
 }
 
-// get user 
 
+/*
+Link to API (Twitter/mapbox geocode)
+*/
+// get user 
 function getUser() {    
     return $.ajax({
         url: "/searchUserName?username=GuellaRoxane",
@@ -168,6 +229,7 @@ function getUser() {
     })
 }
 
+// Get tweest from user
 function getTweetsUser(userId) {
     return $.ajax({
         url: `/userTweets?id=${userId}`,
@@ -193,6 +255,7 @@ function getGeocode(accessToken,adress) {
     })
 }
 
+//get rewteets of a specific tweet
 function quotedOf(tweetId) {
     return $.ajax({
         url: `/quotedOf?id=${tweetId}`,
